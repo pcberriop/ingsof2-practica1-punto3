8a7384efe9113e4008fbffc3b545a0564fd66eed
Pedro Berrio <pcberriop@unal.edu.co>
2017-09-02 16:22:45 +0000

    correcion de un error en user

--- app/models/estudiante.rb
+++ app/models/estudiante.rb
@@ -1,6 +1,6 @@
 class Estudiante < ApplicationRecord
     validates :nombre, length: { maximum: 20}, presence: true
-    validates :nota_uno, presence: true, length: { is: 6}, uniqueness:true
-    validates :nota_dos, length: { maximum: 10}, presence: true
-    validates :model, presence: true, length: { maximum: 20}
+    validates :nota_uno, presence: true, length: { maximum: 3}
+    validates :nota_dos, length: { maximum: 3}, presence: true
+    validates :definitiva, presence: true, length: { maximum: 3}
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,5 @@
 class Pet < ApplicationRecord
+    validates :name, length: { maximum: 20}, presence: true
+    validates :type, presence: true, length: { maximum: 3}
+    validates :owner, length: { maximum: 3}, presence: true
 end
